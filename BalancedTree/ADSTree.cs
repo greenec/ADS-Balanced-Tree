cf8132399a6c24fe8b884594d378328f1aab7f89
Connor Greene <connor.greene2000@gmail.com>
2018-04-12 11:19:56 -0400

    Initial commit

---
+++ BalancedTree/ADSTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedTree
+{
+    public enum TraverseOrder
+    {
+        InOrder,
+        PreOrder,
+        PostOrder
+    }
+
+    class ADSTree
+    {
+        private ADSNode root;
+
+        public sealed class ADSNode
+        {
+            public ADSNode left;
+            public ADSNode right;
+            public int key;
+            public int cardinality;  //  Increment each time duplicates are added
+            public int height;  // Height of this node
+        }
+
+        public ADSTree()
+        {
+            root = null;
+        }
+
+        // Return the node where value is located
+        public ADSNode Find(int value)
+        {
+            return null;
+        }
+
+        // Inserts a node into the tree and maintains it's balance
+        public void Insert(int value)
+        {
+
+        }
+
+        // Print the tree in a particular order
+        public void PrintTree(TraverseOrder order)
+        {
+
+        }
+    }
+}
+
---
+++ BalancedTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new ADSTree();
+
+            tree.Insert(43);
+            tree.Insert(18);
+            tree.Insert(22);
+            tree.Insert(9);
+            tree.Insert(21);
+            tree.Insert(6);
+            tree.Insert(8);
+            tree.Insert(20);
+            tree.Insert(63);
+            tree.Insert(50);
+
+            tree.PrintTree(TraverseOrder.InOrder);
+        }
+    }
+}
